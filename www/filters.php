6f2088bdfdac3e63eb6ee526424d05224f32652d
re.kovalev <re.kovalev@gmail.com>
2022-03-31 13:41:15 +0300

    min-max filters

--- www/filters.php
+++ www/filters.php
@@ -11,7 +11,7 @@ $filter_data = $DB->query($db_request)->fetchAll();
 
 
 $request_body = file_get_contents('php://input');
-
+// echo $request_body;
 $json_data = json_decode($request_body, true);
 
 $db_request = 'SELECT meas.id, species.name species, publication.name publication, mmr_method.name mmr_method, 
@@ -36,15 +36,38 @@ foreach($filter_data as $row)
             $db_request .= ' WHERE ';
         } 
         else
-        $db_request .= ' AND ';
+            $db_request .= ' AND ';
         $db_request .= str_replace(array('%JSON_ID%', '%VALUE%'),
                                    array($row['json_ident'], $json_data[$row['json_ident']]),
                                    $row['sql_code']);
     }
+    if (array_key_exists($row['json_ident'].'__min', $json_data))
+    {
+        if ($where_flag == 0)
+        {
+            $where_flag = 1;
+            $db_request .= ' WHERE ';
+        } 
+        else
+            $db_request .= ' AND ';
+        $db_request .= $json_data[$row['json_ident'] . '__min'] . ' <= meas.' . $row['json_ident'];
+    }
+    if (array_key_exists($row['json_ident'].'__max', $json_data))
+    {
+        if ($where_flag == 0)
+        {
+            $where_flag = 1;
+            $db_request .= ' WHERE ';
+        } 
+        else
+            $db_request .= ' AND ';
+        $db_request .= 'meas.' . $row['json_ident'] . ' <= ' . $json_data[$row['json_ident'] . '__max'];
+    }
 }
 
-
+// echo $db_request;
 $stmt = $DB->query($db_request);
+
 $comma_flag = 0;
 echo "{\r\n";
 echo "  \"count\":" . $stmt->rowCount() . ",\r\n";
